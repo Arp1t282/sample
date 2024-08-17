9b6ce6c156265cccaf96cf6afa17b01a100ed9e5
Arpit <arpitkv1002@gmail.com>
2024-08-17 13:18:36 +0530

    Added my project

---
+++ src/test/java/com/e2e/sample/SampleApplicationTests.java
@@ -0,0 +1,14 @@
+package com.e2e.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+
+	void contextLoads() {
+	}
+
+}
